fc2122a784771fd87bb0b5cb704b0acf387012ea
Shane Holloway <shane.holloway@ieee.org>
2020-05-04 16:51:31 -0600

    Working rtc with mqtt and nice api

--- code/idrnd36.mjs
+++ code/idrnd36.mjs
@@ -1,7 +1,9 @@
-export const create_idrnd36 = (n, sep='-') =>
+export const create_crnd36 = (n, sep='-') =>
   Array.from(
     crypto.getRandomValues(new Uint32Array(n || 1)),
     v => v.toString(36),
   ).join(sep)
 
 
+export const create_rnd36 = () =>
+  Math.random().toString(36).slice(2)
